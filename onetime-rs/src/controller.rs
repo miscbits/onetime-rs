086fb6cbf2a3304bcbcff4629b24641a678de339
Wilhem Alcivar <alcivaw@wz.hasbro.com>
2023-10-07 19:16:55 -0700

    encrypting content at rest

--- onetime-rs/src/controller.rs
+++ onetime-rs/src/controller.rs
@@ -1,8 +1,9 @@
+use crate::payload::CreateSecretResponse;
 use crate::payload::CreateSecretPayload;
+use crate::payload::QueryPassword;
 use crate::model::Secret;
 use crate::bb8::PooledConnection;
 use crate::bb8::Pool;
-
 use uuid::Uuid;
 
 use bb8_redis::{
@@ -15,8 +16,15 @@ use axum::{
     http::{request::Parts, StatusCode},
     response::IntoResponse,
     Json,
-    extract::{Path, FromRequestParts, FromRef},
+    extract::{Path, FromRequestParts, FromRef, Query},
 };
+
+use aes_gcm_siv::{
+    aead::{Aead, KeyInit},
+    Aes256GcmSiv, Nonce
+};
+extern crate argon2;
+use rand::distributions::{Alphanumeric, DistString};
 
 type ConnectionPool = Pool<RedisConnectionManager>;
 
@@ -39,17 +47,26 @@ where
     }
 }
 
-pub async fn get_secret(Path(secret_id): Path<Uuid>,
-        DatabaseConnection(mut conn): DatabaseConnection,) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
-    let query_response: Result<String, RedisError> = conn.get_del(secret_id.to_string()).await;
+pub async fn get_secret(Path(secret_id): Path<Uuid>, DatabaseConnection(mut conn): DatabaseConnection,
+                 payload: Query<QueryPassword>,
+        ) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
+    let query_response: Result<Vec<u8>, RedisError> = conn.get(secret_id.to_string()).await;
     // let secret_content = conn.get_del(secret_id.to_string()).await.map_err(not_found).unwrap();
     match query_response {
-        Ok(secret_content) => {
+        Ok(encrypted_secret_content) => {
+            let hashed_password = hash_password(payload.password.clone());
+            let cipher = Aes256GcmSiv::new((&hashed_password).into());
+
+            let nonce_string: String = conn.get(secret_id.to_string() + "_nonce").await.unwrap();
+            let nonce = Nonce::from_slice(nonce_string.as_bytes());
+
+            let secret_content = cipher.decrypt(nonce, encrypted_secret_content.as_ref()).unwrap();
+
             let secret = Secret {
                 id: secret_id,
-                secret_content: secret_content
+                secret_content: String::from_utf8(secret_content).unwrap()
             };
-
+            let _ : () = conn.del(secret_id.to_string()).await.unwrap();
             Ok((StatusCode::OK, Json(secret)))
         },
         Err(_) => Err((StatusCode::NOT_FOUND, Json(serde_json::Value::String(String::from("The secret you are looking for could not be located")))))
@@ -58,13 +75,22 @@ pub async fn get_secret(Path(secret_id): Path<Uuid>,
 }
 
 pub async fn create_secret(DatabaseConnection(mut conn): DatabaseConnection,
-    Json(payload): Json<CreateSecretPayload>,) -> (StatusCode, Json<Secret>) {
-    let secret = Secret {
+    Json(payload): Json<CreateSecretPayload>,) -> (StatusCode, Json<CreateSecretResponse>) {
+
+    let hashed_password = hash_password(payload.password);
+    let cipher = Aes256GcmSiv::new((&hashed_password).into());
+    let random_nonce_string = Alphanumeric.sample_string(&mut rand::thread_rng(), 12);
+    let nonce = Nonce::from_slice(random_nonce_string.as_bytes());
+
+    let ciphertext = cipher.encrypt(nonce, payload.secret_content.as_ref()).unwrap();
+
+    let secret = CreateSecretResponse {
         id: Uuid::new_v4(),
-        secret_content: payload.secret_content
+        url: String::from("path/to/uuid/url")
     };
 
-    let _ : () = conn.set(secret.id.to_string(), &secret.secret_content).await.unwrap();
+    let _ : () = conn.set(secret.id.to_string() + "_nonce", random_nonce_string).await.unwrap();    
+    let _ : () = conn.set(secret.id.to_string(), ciphertext).await.unwrap();
     let _ : () = conn.expire(secret.id.to_string(), 86400).await.unwrap();
     (StatusCode::CREATED, Json(secret))
 }
@@ -77,3 +103,26 @@ where
 {
     (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
 }
+
+fn argon2_config<'a>() -> argon2::Config<'a> {
+    return argon2::Config {
+        variant: argon2::Variant::Argon2id,
+        hash_length: 32,
+        lanes: 8,
+        mem_cost: 16 * 1024,
+        time_cost: 8,
+        ..Default::default()
+    };
+}
+
+fn hash_password(password: String) -> [u8; 32]  {
+    let salt = b"randomsalt";
+    let argon2_config = argon2_config();
+
+    pop(&argon2::hash_raw(password.as_bytes(), salt, &argon2_config).unwrap())
+}
+
+fn pop(v: &[u8]) -> [u8; 32]{
+    v.try_into().expect("slice with incorrect length")
+}
+
--- onetime-rs/src/payload.rs
+++ onetime-rs/src/payload.rs
@@ -1,6 +1,19 @@
-use serde::{Deserialize};
+use uuid::Uuid;
+use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct CreateSecretPayload {
-    pub secret_content: String
+    pub secret_content: String,
+    pub password: String
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct CreateSecretResponse {
+    pub id: Uuid,
+    pub url: String
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct QueryPassword {
+    pub password: String,
 }
